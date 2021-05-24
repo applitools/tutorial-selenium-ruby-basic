a48ecb6971d587781d981c78e79e527bfc24b16f
artem <artem.borodavka@applitools.com>
2021-05-24 10:58:20 +0300

    update regexp

--- updateGemfile.sh
+++ updateGemfile.sh
@@ -1,4 +1,3 @@
-VERSION=$(gem search -e eyes_selenium | grep eyes_selenium | grep -E -o "\d+.\d+.\d+")
-gem search -e eyes_selenium
+VERSION=$(gem search -e eyes_selenium | grep eyes_selenium | grep -E -o "[0-9]+.[0-9]+.[0-9]+")
 echo "$VERSION"
 bundle remove eyes_selenium && bundle add eyes_selenium -v "$VERSION"
