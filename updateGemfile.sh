b31f048435565c2c0b3562eaa0a51f379f7f108e
artem <artem.borodavka@applitools.com>
2021-05-24 10:52:19 +0300

    update logging

--- updateGemfile.sh
+++ updateGemfile.sh
@@ -1,3 +1,4 @@
 VERSION=$(gem search -e eyes_selenium | grep eyes_selenium | grep -E -o "\d+.\d+.\d+")
-echo $VERSION
+gem search -e eyes_selenium
+echo "$VERSION"
 bundle remove eyes_selenium && bundle add eyes_selenium -v "$VERSION"
